f7b248fc7765d2e359f442b7a686a03569516b85
duckcraft7 <duckcraft210@gmail.com>
2025-11-11 20:08:46 -0300

    Initialize Android project with Gradle KTS setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,38 @@
 
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android") version "1.9.0" // Mesmo que estejamos em Java, o KTS usa
+}
+
+android {
+    namespace = "com.meuapp.emulato"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.meuapp.emulato"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    // Informa ao Gradle onde encontrar os arquivos
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/java")
+            res.srcDirs("src/main/res")
+            manifest.srcFile("src/main/AndroidManifest.xml")
+        }
+    }
+}
+
+dependencies {
+    // Dependência necessária para o modo de tela cheia
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
